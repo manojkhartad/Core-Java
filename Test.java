c1fcf7229cbc8b25c21da728948ad47954160e44
Manoj Khartad <manojkhartad@gmail.com>
2024-09-19 20:42:07 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 65;
+		float b = a;
+		System.out.println(b);
+		
+		char ch = 'A';
+		int x = ch;
+		System.out.println(x);
+		
+		int y = 69;
+		char ch1 = (char) y;
+		System.out.println(ch1);
+
+	}
+
+}
