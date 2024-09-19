31b9af27dbb28879f32b1fdd56db829acbdf9998
Manoj Khartad <manojkhartad@gmail.com>
2024-09-19 19:22:35 +0530

    Add files via upload

---
+++ DaysToHours.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DaysToHours {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Days");
+		int days = scan.nextInt();
+		int hours = days * 24;
+		int min = days / 24 / 60;
+		int sec = days / 24 / 60 / 60;
+		System.out.println(hours + "Hrs: " + min + "m: " + sec + "s:");
+		
+
+	}
+
+}
