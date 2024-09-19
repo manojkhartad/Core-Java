587894c4e7a2a7414fa7b5fbd71bdbaee51fb943
Manoj Khartad <manojkhartad@gmail.com>
2024-09-19 19:19:52 +0530

    Add files via upload

---
+++ CelciusToFarenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CelciusToFarenheit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Enter Celcius Value");
+		double c = scan.nextDouble();
+		double f = (9/5) * c + 32;
+		System.out.println("Farenheit = " +f);
+		
+	}
+
+}
