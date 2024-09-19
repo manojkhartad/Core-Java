13ad5f1cd73169e25f751d00e17316c3540f83ac
Manoj Khartad <manojkhartad@gmail.com>
2024-09-19 19:13:01 +0530

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,5 @@
 /* Write a program to print Zip Zap and Jar. If the number is divisible by 3 print Zip, if number is divisible by 5 print Zap, if number is divisible by 3 and 5 both
-/*print Jar 
+*print Jar 
 */
 import java.util.Scanner;
 
