1b123a5834749505acd923f4ccb4d4f3d7637d5a
Manoj Khartad <manojkhartad@gmail.com>
2024-09-19 19:18:35 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the size of an array");
+		int n = scan.nextInt();
+		// create an array
+		int arr[] = new int[n];
+		for(int i = 0; i < n; i++ ) {
+			System.out.println("Enter the value at index: " + i);
+			arr[i] = scan.nextInt();
+		}
+		findSum(arr); // Calling method
+	}
+	private static void findSum(int arr[]) { //  Method Defination
+		int sum = 0;
+		for ( int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+		}
+		System.out.println("The sum of Array value is" + " " + sum);
+	}
+
+}
