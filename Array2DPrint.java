307b8920bb68838450e4c58430ab815a88d5cffa
Manoj Khartad <manojkhartad@gmail.com>
2024-09-19 19:09:25 +0530

    Add files via upload

---
+++ Array2DPrint.java
@@ -0,0 +1,19 @@
+
+public class Array2DPrint {
+
+	public static void main(String[] args) {
+
+			int[][] arr = { { 2, 5, 8 }, { 4, 1, 5 }, { 9, 7, 3 } };
+			
+			for ( int i = 0; i < arr.length; i++) {
+				
+				for ( int j = 0; j < arr[0].length; j++) {
+					
+					System.out.print(arr[i][j] + " ");
+					
+				}
+				System.out.println();
+			}
+	}
+
+}
