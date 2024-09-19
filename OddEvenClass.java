a4be60d2d6ea13d2039a05b3599a3588ac73bed0
Manoj Khartad <manojkhartad@gmail.com>
2024-09-19 18:42:02 +0530

    Add files via upload

---
+++ OddEvenClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OddEvenClass {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int x = scan.nextInt();
+		if ( x % 2 == 0)
+		{
+			System.out.println( x + " : Even Number");
+			
+		}
+		else
+		{
+			System.out.println( x+ " : Odd Number");
+		}
+	}
+
+}
